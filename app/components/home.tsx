45c45bcbe903e31ee41f1fc858a1463383352bc9
chengithubcangku <31264006+chengithubcangku@users.noreply.github.com>
2023-04-03 11:13:47 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -156,7 +156,7 @@ export function Home() {
             </div>
             <div className={styles["sidebar-action"]}>
               <a href={REPO_URL} target="_blank">
-                <IconButton icon={<QQIcon />} shadow />
+                <IconButton icon={<QQIcon />
               </a>
             </div>
           </div>
